70cb5dce7bd76c3ac757d026d602382f63f12715
Doug <doug.a.crawford@gmail.com>
2024-12-20 12:16:47 -0800

    Conditionally render navigation based on user authentication status

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { faGripLines } from "@fortawesome/free-solid-svg-icons";
+import { faGripLines, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Link } from "react-router";
@@ -10,35 +10,45 @@ import Modal from "~/components/modal/modal";
 export default function Header() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  console.log(user);
+  console.log(user?.name);
   console.log(isAuthenticated);
 
   return (
     <header className="grid justify-end m-4">
-      <Modal
-        basic={false}
-        modalName="navigation"
-        triggerElement={<FontAwesomeIcon icon={faGripLines} size="2xl" />}
-      >
-        <nav className="grid gap-2 text-center">
-          <Link to="/" className="bg-blue-600/80 block rounded p-2">
-            Home
-          </Link>
-          {isAuthenticated ? (
-            <>
-              <LogoutButton className="bg-blue-600/80 block rounded p-2" />
-              <Link
-                className="bg-blue-600/80 block rounded p-2"
-                to="/observations"
-              >
-                View Observations
-              </Link>
-            </>
-          ) : (
-            <LoginButton className="bg-blue-600/80 block rounded p-2" />
-          )}
-        </nav>
-      </Modal>
+      {isLoading ? (
+        <FontAwesomeIcon icon={faSpinner} spin size="2xl" />
+      ) : (
+        <Modal
+          basic={false}
+          modalName="navigation"
+          triggerElement={<FontAwesomeIcon icon={faGripLines} size="2xl" />}
+        >
+          <nav className="grid gap-2 text-center">
+            <Link to="/" className="bg-blue-600/80 block rounded p-2">
+              Home
+            </Link>
+            {isAuthenticated ? (
+              <>
+                <div>
+                  <p>Logged in as:</p>
+                  <p>{user?.name}</p>
+                </div>
+                <LogoutButton className="bg-blue-600/80 block rounded p-2" />
+                <Link
+                  className="bg-blue-600/80 block rounded p-2"
+                  to="/observations"
+                >
+                  View Observations
+                </Link>
+              </>
+            ) : (
+              <>
+                <LoginButton className="bg-blue-600/80 block rounded p-2" />
+              </>
+            )}
+          </nav>
+        </Modal>
+      )}
     </header>
   );
 }
